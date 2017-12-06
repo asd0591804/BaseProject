0869bad3affa46c00c60bfe1be86b4a851327102
asd0591804 <lestia521328@gmail.com>
2017-12-06 12:37:34 +0800

    Test

---
+++ src/main/java/com/github/asd0591804/test.java
@@ -0,0 +1,10 @@
+package com.github.asd0591804;
+
+public class test {
+    public int test(int num){
+        if(num<10){
+            System.out.print("number");
+        }
+        return num;
+    }
+}
---
+++ src/test/java/com/github/asd0591804/testTest.java
@@ -0,0 +1,13 @@
+package com.github.asd0591804;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class testTest {
+    @Test
+    public void test1() throws Exception {
+        assertEquals(0,0);
+    }
+
+}
